f03b304849d965ed23be76cd0bdce2f3aae3f011
alpaylan <alpkeles99@gmail.com>
2024-12-31 02:16:37 -0500

    add syntax module that will be used to implement different mutation syntaxes

---
+++ src/algebra.rs
@@ -0,0 +1,14 @@
+
+type Tag = String;
+type Id = String;
+
+pub(crate) enum Expr {
+    Sum(Box<Expr>, Box<Expr>),
+    Product(Box<Expr>, Box<Expr>),
+    USum(Tag),
+    UProduct(Tag),
+    Id(Id),
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@
 use clap::Parser;
 use cli::Opts;
 
+mod algebra;
 mod cli;
 mod code;
 mod languages;
+mod syntax;
 mod variation;
-
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
 
